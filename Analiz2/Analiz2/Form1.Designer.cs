9cead11948f87ac0a4926fccba6a9551929d4764
FIRDEVS-ALTUNTAS <114739011+FIRDEVS-ALTUNTAS@users.noreply.github.com>
2024-08-19 16:06:42 +0300

    Add files via upload

---
+++ Analiz2/Analiz2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Analiz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.pdfButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(21, 41);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(258, 26);
+            this.filePathTextBox.TabIndex = 0;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(21, 77);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(258, 193);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.Text = "";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loadButton.Location = new System.Drawing.Point(310, 41);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(130, 35);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Dosya Yükle";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.analyzeButton.Location = new System.Drawing.Point(310, 82);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(132, 35);
+            this.analyzeButton.TabIndex = 3;
+            this.analyzeButton.Text = "Analiz";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // pdfButton
+            // 
+            this.pdfButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pdfButton.Location = new System.Drawing.Point(310, 123);
+            this.pdfButton.Name = "pdfButton";
+            this.pdfButton.Size = new System.Drawing.Size(130, 35);
+            this.pdfButton.TabIndex = 4;
+            this.pdfButton.Text = "Word Dosyası ";
+            this.pdfButton.UseVisualStyleBackColor = true;
+            this.pdfButton.Click += new System.EventHandler(this.pdfButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(460, 301);
+            this.Controls.Add(this.pdfButton);
+            this.Controls.Add(this.analyzeButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.filePathTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Analizci";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Button pdfButton;
+    }
+}
+
---
+++ Analiz2/Analiz2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Analiz2
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePathTextBox.Text = openFileDialog.FileName;
+                filePath = openFileDialog.FileName;  // Yolu sakla
+            }
+        }
+
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                // Python scriptini çağırarak analiz yapalım
+                RunPythonScript(filePath);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir dosya yolu girin.");
+            }
+        }
+
+        private void pdfButton_Click(object sender, EventArgs e)
+        {
+            // Word dosyasının oluşturulup oluşturulmadığını kontrol et ve dosyayı indir
+            CreateWordDocument();
+        }
+
+        private void RunPythonScript(string filePath)
+        {
+            string pythonExePath = @"C:\Users\Firdevs\anaconda3\python.exe";
+            string scriptPath = @"C:\Users\Firdevs\PycharmProjects\pythonProject1\AnalizUygulama.py";
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = pythonExePath;
+            start.Arguments = $"{scriptPath} \"{filePath}\"";
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            start.CreateNoWindow = true;
+
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    resultTextBox.Text = result;
+                }
+                using (StreamReader reader = process.StandardError)
+                {
+                    string error = reader.ReadToEnd();
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        MessageBox.Show("Hata oluştu: " + error);
+                    }
+                }
+            }
+        }
+
+        private void CreateWordDocument()
+        {
+            string wordSourcePath = @"C:\Users\Firdevs\PycharmProjects\pythonProject1\analysis_report.docx";
+            string wordDestinationPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "analysis_report.docx");
+
+            if (File.Exists(wordSourcePath))
+            {
+                File.Copy(wordSourcePath, wordDestinationPath, true);
+                MessageBox.Show($"Word belgesi indirildi: {wordDestinationPath}");
+            }
+            else
+            {
+                MessageBox.Show("Word belgesi bulunamadı. Lütfen analiz tamamlandıktan sonra tekrar deneyin.");
+            }
+        }
+    }
+}
